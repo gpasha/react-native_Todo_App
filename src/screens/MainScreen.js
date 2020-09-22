abf768e4063e4a10150fa6774d053389e63fca51
pavel.glebik <pavel.glebik@jetbi.com>
2020-09-22 23:26:07 +0300

    third section v.2

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,26 +1,54 @@
 import React from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
+import { View, StyleSheet, FlatList, Image } from 'react-native'
 import { AppTodo } from '.././components/AppTodo'
 import { Todo } from '.././components/Todo'
 
 export const MainScreen = ({ addTodo, removeTodo, todos, openTodo }) => {
-    return (
-        <View>
-            <AppTodo onSubmit={addTodo} />
-            <FlatList
-                style={styles.containerList}
-                data={todos}
-                renderItem={({ item }) => (
-                    <Todo id={item.id} title={item.title} remove={removeTodo} openTodo={openTodo} />
-                )}      
-                keyExtractor={item => item.id}
-            />
-        </View>
-      )
+
+  let content = (
+    <FlatList
+      style={styles.containerList}
+      data={todos}
+      renderItem={({ item }) => (
+          <Todo id={item.id} title={item.title} remove={removeTodo} openTodo={openTodo} />
+      )}      
+      keyExtractor={item => item.id}
+    />
+  )
+
+  if ( todos.length === 0 ) {
+    content = (
+      <View style={styles.imageBlock} >
+        <Image style={styles.image} source={require('../../assets/empty_list.jpg')} />
+        {/* <Image style={styles.image} source={{ uri: 'https://cdn.onlinewebfonts.com/svg/img_60937.png' }} />         */}
+      </View>
+    )
+  }
+
+  return (
+      <View>
+          <AppTodo onSubmit={addTodo} />
+
+          { content }
+          
+      </View>
+    )
 }
 
 const styles = StyleSheet.create({
   containerList: {
     maxHeight: '80%'
+  },
+  imageBlock: {
+    width: '100%',
+    height: '80%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    width: '80%',
+    height: '80%',
+    resizeMode: 'contain',
+    opacity: 0.2
   }
 });
